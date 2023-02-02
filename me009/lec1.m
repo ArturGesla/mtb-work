5232379d9596b6dee34b3800ab9f4ffe5d539479
dzou <dzou@company.com>
2023-02-02 10:43:14 +0100

    lec1

--- me009/lec1.m
+++ me009/lec1.m
@@ -16,3 +16,21 @@ for i=1:200
     u=v;
 end
 %%
+n=4;
+A=eye(n-1)*2-diag(ones(n-2,1),-1)-diag(ones(n-2,1),1);
+e1=min(eig(A))*n^2
+pi-sqrt(e1)
+
+n=8;
+A=eye(n-1)*2-diag(ones(n-2,1),-1)-diag(ones(n-2,1),1);
+e2=min(eig(A))*n^2
+pi-sqrt(e2)
+
+n=16;
+A=eye(n-1)*2-diag(ones(n-2,1),-1)-diag(ones(n-2,1),1);
+e3=min(eig(A))*n^2
+% 
+% % (e2-e1)/(e3-e2)
+% % (sqrt(e2)-sqrt(e1))/(sqrt(e3)-sqrt(e2))
+(pi-sqrt(e1))/(pi-sqrt(e2))
+

897fa829e713990d1b6f4c0bb50367a2a3fac1fd
dzou <dzou@company.com>
2024-01-16 21:53:02 +0100

    noack

--- sn-stab/sn-noack/main_tests.m
+++ sn-stab/sn-noack/main_tests.m
@@ -15,8 +15,8 @@ sigma=1;
 
 x=exp(sigma*mod(t,1));
 A0=1/sigma*(exp(sigma)-1);
-An=@(n) 2*(sigma/n^2/pi^2/4)/(1+sigma^2/n^2/pi^2/4)*(exp(sigma)*(1)^n-1);
-Bn=@(n) An(n)*2*n*pi/(-sigma);
+An=@(n) 2*(sigma./n.^2/pi^2/4)./(1+sigma^2./n.^2/pi^2/4).*(exp(sigma)*(1).^n-1);
+Bn=@(n) An(n)*2.*n*pi/(-sigma);
 
 
 
@@ -44,3 +44,20 @@ xc=ifft(zc)*length(zc);
 plot(t,xc,'-x');
 
 % clf; plot(xn-xc)
+%%
+semilogy(An(1:40),'-x'); hold on; semilogy(-Bn(1:40),'-x'); semilogy(real(z(2:end))*2,'-x'); semilogy(imag(z(2:end))*2,'-sq'); xlim([0,40])
+
+%%
+
+x=0:0.01:2-0.01; y=sin(2*x*pi); ya=exp(x);
+z=fftshift(fft(y))/length(y);
+za=fftshift(fft(ya))/length(ya);
+% z2=conv(z,z,'same');
+z2=conv(z,za,'same');
+y2=ifft(fftshift(z2))*length(z2); norm(imag(y2))
+y2=real(y2);
+
+%%
+plot(x,[y;y.^2;y2; y.*ya]'); grid on;
+
+

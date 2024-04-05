5b657aa3e4747a196a4f6b657d65ff7f7b1bfbd5
artur gesla <agesla09e@gmail.com>
2024-04-05 11:32:23 +0200

    auto commit

---
+++ ai-intro/main-pt3.py
@@ -0,0 +1,12 @@
+#%%
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy as scp
+from scipy import stats
+import statsmodels
+#%
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+#%%

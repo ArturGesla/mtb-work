a904351a7bb6d243c171a276c07d029f756d8916
artur gesla <agesla09e@gmail.com>
2024-06-28 16:47:55 +0200

    auto commit

---
+++ lingwood2/vonkarman-bvp4c-rotframe/main.m
@@ -0,0 +1,47 @@
+%%
+clc;
+xmesh = linspace(0,20,100);
+solinit = bvpinit(xmesh, @guess);
+
+sol = bvp4c(@bvpfcn, @bcfcn, solinit);
+
+% plot(sol.x, sol.y, '-')
+clf;
+plot(sol.y(1:3,:), sol.x, 'o')
+
+save('vk.mat')
+%%
+a=load("../vonkarman-bvp4c/vk.mat"); 
+
+hold on;
+plot(a.sol.y(1:3,:)-[0;1;0], a.sol.x,"-")
+%%
+function dydx = bvpfcn(x,y) % equation to solve
+dydx = zeros(5,1);
+dydx = [y(4)
+       y(5)
+       -2*y(1)
+       y(1)^2-y(2)^2+y(4)*y(3)-2*y(2)-1
+       % y(1)^2-y(2)^2+y(4)*y(3)+1 %bode
+       2*y(1)*y(2)+y(5)*y(3)+2*y(1)
+       % 2*y(1)*y(2)+y(5)*y(3) %bode
+       ];
+end
+%--------------------------------
+function res = bcfcn(ya,yb) % boundary conditions
+res = [ya(1)
+    ya(2)
+    ya(3)
+    yb(1)
+    yb(2)+1 ];
+end
+%--------------------------------
+function g = guess(x) % initial guess for y and y'
+g = 1*[x*0
+     x*0-1
+     x*0-1
+     x*0
+     x*0];
+end
+%-------------------------
+

79b34ca1ef91f3a66448397f123a80b104c6bbf4
dzou <dzou@company.com>
2024-07-13 21:29:27 +0200

    msg

--- lingwood2/vonkarman-fd-specPstag-rotframe/equiv6th/main.m
+++ lingwood2/vonkarman-fd-specPstag-rotframe/equiv6th/main.m
@@ -21,9 +21,23 @@ alpha=1;
 
 [g,jac0,jac1,jac2]=evalJacRhsStab(u,x,U,omega,bbar,R,alpha);
 
+R=515; bbar=0.0117;
+% beta=0; bbar=beta/R;
+
+
 %%
-eva=[];
-omega=0.01i; omega=-0.025+0.004i; omega=-0.025+0.0005i; omega=-0.025-0.001i;
+
+evaM=[];
+%%
+% omega=0.01i/2/2; 
+
+% R=515; bbar=0.0117; omega=-0.025+0.004i; omega=-0.025+0.0005i; omega=-0.025-0.001i;
+% R=530; beta=67; bbar=beta/R; omega=-0.025*2+0.1i/2/2/2/2/2/2/2; %omega=-0.025+0.0005i; omega=-0.025-0.001i;
+
+R=530; beta=67; bbar=beta/R; omega=-0.025*2+0.1i/2/2/2/2/2/2/2; %omega=-0.025+0.0005i; omega=-0.025-0.001i;
+
+ns=100; dom=0.01/2/2/4; om0r=-0.03125; om0i=4e-4; a0r=0.22; a0i=-0.14;
+oma=linspace(om0r-ns/2*dom,om0r+ns/2*dom,ns)+om0i*1i;
 % oma=-0.0692 :0.002/4:0.0262; oma1=oma+0.04i;
 % oma=-0.0692 :0.002/4:0.0262; oma2=oma+0.015i;
 % % om0=-0.0692; om1=0.0262; omx=-1:2/50:1; omx=omx.^3; omx=(omx+1)/2*(om1-om0)+om0; oma3=omx+0.0133i;
@@ -32,24 +46,29 @@ omega=0.01i; omega=-0.025+0.004i; omega=-0.025+0.0005i; omega=-0.025-0.001i;
 % ev=eig(full(jac0),-full(jac1));
 
 %%
+eva=[]; im=[];
 for i=1:120;%120
-R=515; bbar=0.0117;
 % omega=0.01i;
 
 [g,jac0,jac1,jac2]=evalJacRhsStab(u,x,U,omega,bbar,R,alpha);
 % tic; [evc,evs]=polyeig(jac0,jac1,jac2); ev=(evs); toc; 
-tic; [evc,evs]=polyeigs2(jac0,jac1,jac2,20,0.25); ev=diag((evs)); toc; 
-eva=[eva,ev]; 
+tic; [evc,evs]=polyeigs2(jac0,jac1,jac2,20,0.2); ev=diag((evs)); toc; 
+eva=[eva;ev]; im=[im;ev*0+omega];
 omega=omega+0.01/2/2/4%/2
 i
 end
+evaM=[evaM,eva];
 %%
 clf;
-plot(eva,'k.'); hold on;
+% plot(eva,'k.'); hold on;
+% plot(eva,'k.'); hold on; text(real(eva),imag(eva),num2str(im))
+plot(evaM,'.'); hold on;
 % plot(eva,'x'); hold on;
  plot(ev,'x'); hold on; text(real(ev),imag(ev),num2str([1:length(ev)]'))
 % plot(ev,'o');
-xlim([0 0.35]); ylim([-0.2 0.2]*2); grid on;
+% xlim([0 0.35]);
+ylim([-0.2 0.2]*2); 
+grid on;
 % xlim([-0.1 1.5]); ylim([-1 1]); grid on;
 % xlim([-0.1 0.5]*10); ylim([-0.5 0.5]*10); grid on;
 % xlim([-0.1 0.5]*40); ylim([-0.5 0.5]*40)
@@ -64,7 +83,7 @@ xlim([0 0.35]); ylim([-0.2 0.2]*2); grid on;
 
 %%
 clf;
-iev=12;
+iev=3;
 up=reshape(real(evc(:,iev)),[4,length(x)])';
 plot(up(:,1:end),x,'x-'); grid on;
 % plot(up(:,1:1),x,'x-'); grid on;
@@ -77,18 +96,23 @@ plot(x,real(U(2:4:end)*ev(iev)+bbar*U(3:4:end)-omega)); hold on;  grid on;
 plot(x,imag(U(2:4:end)*ev(iev)+bbar*U(3:4:end)-omega)); hold on;  grid on;
 %%
 eva=[];
-oma=oma2; lam1=0.25-0.5i; evaa=[lam1];
-for i=1:1;%length(oma)
+% oma=oma2; lam1=0.25-0.5i; evaa=[lam1];
+for i=1:length(oma)
 %     for i=40:length(oma)
 omega=oma(i);
-[g,jac0,jac1,jac2,jac3]=evalJacRhsStab(u,x,U,omega,bbar,R,alpha);
-% [evc,evs]=polyeig(jac0,jac1,jac2,jac3); ev=(evs);
-[evc,evs]=polyeigs(jac0,jac1,jac2,jac3,1,evaa(end)); ev=(evs); evaa(end+1)=ev(1);
-% [evc,evs]=eig(full(jac0),-full(jac1)); ev=diag(evs);
-% [evc,evs]=eigs((jac0),-(jac1),20,'smallestabs'); ev=diag(evs);
+[g,jac0,jac1,jac2]=evalJacRhsStab(u,x,U,omega,bbar,R,alpha);
+% tic; [evc,evs]=polyeig(jac0,jac1,jac2); ev=(evs); toc; 
+% tic; 
+[evc,evs]=polyeigs2(jac0,jac1,jac2,4,a0r+a0i*1i); ev=diag((evs));
+% toc; 
 eva=[eva,ev];
 i
 end
+%%
+clf;
+plot(eva,'.'); %hold on;
+grid on;
+
 
 %%
 

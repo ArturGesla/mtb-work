8febb39785996e0dbed9bc1a01ac9ed9d19158c5
dzou <dzou@company.com>
2024-10-27 22:05:03 +0100

    msg

--- lingwood2/rs-regP/stab/ts_plot.m
+++ lingwood2/rs-regP/stab/ts_plot.m
@@ -1,5 +1,5 @@
 %%
-clf; 
+
 set(gcf,"Position",[          24         267        1306         399]);
 a1=load("resarrto148.mat"); a=a1.resarr(:,2:end);
 a1=load("resarrto2000.mat"); a=[fliplr(a),a1.resarr(:,2:end)];
@@ -32,4 +32,72 @@ xlabel("Reh"); ylabel("ar"); grid on;
 
 subplot(1,4,4); plot(a(1,:),a(3,:)); title("ai vs Reh at abs inst");
 xlabel("Reh"); ylabel("ai"); grid on;
+
+
+%% 3 curve
+
+clf;
+cd     'C:\Users\Artur\Documents\GitHub\mtb-work\lingwood2\rs-regP\stab'
+
+a1=load("resarrto148.mat"); a=a1.resarr(:,2:end);
+a1=load("resarrto2000.mat"); a=[fliplr(a),a1.resarr(:,2:end)];
+
+ hold on;
+r=0:0.01:10; rlw97=24/sqrt(0.313); reh2=(rlw97./r).^2; plot(r,reh2);
+red=a(4,:); reh=a(1,:); plot(red./sqrt(reh),reh,'-');
+
+%ti
+cd       'C:\Users\Artur\Documents\rotst\self-sim'
+R=[]; ress=[];
+a=load("dataR10.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR8.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR7.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR6.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR5.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR4.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR3.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR2.5.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR2.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR1.7.mat"); R=[R;a.R]; ress=[ress;a.re0];
+
+
+
+
+plot(R,ress,'-x');
+ set(gca,"YTick",[16,100:100:600,900:300:3000]); grid on; hold on;
+% set(gca,"XTick",sort([get(gca,"XTick"),1.7,0.78]))
+set(gca,"XTick",sort([3:1:10,2.5,1.7,0.78]))
+
+ylabel("Reh"); xlabel("r"); xlim([0 10]); ylim([10 3e3]);
+set(gca,"yscale","log");
+legend("assuming Bodewadt profile","taking the real ss profile")
+%%
+%%
+clf;
+cd     'C:\Users\Artur\Documents\rotst\self-sim'
+
+set(gcf,"Position",[   675   100   560   607])
+
+r=0:0.1:10; reclw=(40./r).^2;
+
+semilogy(r,reclw); ylim([10 3000]); set(gca,"YTick",[16,100:100:600,900:300:3000]); grid on; hold on;
+
+R=[]; ress=[];
+a=load("dataR10.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR8.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR7.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR6.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR5.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR4.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR3.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR2.5.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR2.mat"); R=[R;a.R]; ress=[ress;a.re0];
+a=load("dataR1.7.mat"); R=[R;a.R]; ress=[ress;a.re0];
+
+
+
+
+semilogy(R,ress,'-x');
+
+set(gca,"XTick",sort([get(gca,"XTick"),1.6]))
 

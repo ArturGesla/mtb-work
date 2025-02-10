d82b241f9f1505d043d35851c45f8c9da7e478dd
Artur Gesla <gesla@meca43.limsi.fr>
2025-02-10 21:26:11 +0100

    auto commit

--- cheb-diff/main_cheb_2d.m
+++ cheb-diff/main_cheb_2d.m
@@ -1,7 +1,10 @@
 clc; clear;
 uarr=[];
+narr=[];
+%
+nt=3;
 %%
-nt=5+2+2+2+2+2+2+2+2+8;
+nt=nt+2;
 nx=nt; ny=nx;
 % x=linspace(-1,1,nx)';
 % x=x.^3;
@@ -28,7 +31,9 @@ for ix=1+1:length(x)-1
                 J(ip,ipk)=J(ip,ipk)+(dTn2(xc,ikx)*Tn(yc,iky)+dTn2(yc,iky)*Tn(xc,ikx));
             end
         end
-        g(ip)=g(ip)-f(ip);
+%         g(ip)=g(ip)-f(ip);
+%         g(ip)=g(ip)-f(ip)*cos(pi/2*xc)*cos(pi/2*yc);
+%         g(ip)=g(ip)-f(ip)*cos(pi*xc)*cos(pi/2*yc);
 
 
     end
@@ -78,6 +83,11 @@ for iy=1+1:length(y)-1
             J(ip,ipk)=J(ip,ipk)+(Tn(yc,iky)*Tn(xc,ikx));
         end
     end
+%     g(ip)=g(ip)-1; % bc gives 2nd order maybe
+%     g(ip)=g(ip)-(1-y(iy).^2); % bc maybe some 5th
+%     g(ip)=g(ip)-(1-abs(y(iy))); % bc maybe 3rd
+    g(ip)=g(ip)-exp(-1./(1-y(iy).^2)); % bc
+
     ix=nx; ip=iy+(ix-1)*ny;
     for ikx=0:nt-1
         for iky=0:nt-1
@@ -96,6 +106,9 @@ for ix=1+1:length(x)-1
             J(ip,ipk)=J(ip,ipk)+(Tn(yc,iky)*Tn(xc,ikx));
         end
     end
+
+%     g(ip)=g(ip)-1; % bc
+
     iy=ny; ip=iy+(ix-1)*ny;
     for ikx=0:nt-1
         for iky=0:nt-1
@@ -121,16 +134,42 @@ for ix=1:length(x)
         end
     end
 end
+
+uecc=0;
+        for ikx=0:nt-1
+            for iky=0:nt-1
+                ipk=iky+1+(ikx-1+1)*ny;
+                uecc=uecc+u(ipk)*(Tn(0.6,iky)*Tn(0.6,ikx));
+            end
+        end
+
+
 %
 ix=(length(x)+1)/2; iy=(length(y)+1)/2; ip=iy+(ix-1)*ny; 
-uarr=[uarr;uPhys(ip)]
+% uarr=[uarr;uPhys(ip)]
+uarr=[uarr;uecc]
+narr=[narr; nt];
+
 %%
 mesh(reshape(log10(abs(u)),[ny,nx]))
+% mesh(reshape((abs(u)),[ny,nx]))
+%%
+
+clf;
+% semilogy(narr,abs(uarr-2/pi/pi),'x-'); hold on;
+% semilogy(narr,abs(uarr-uarr(end)),'x-'); hold on;
+loglog(narr,abs(uarr-uarr(end)),'x-'); hold on;
+% semilogy(narr,narr.^(-2),'o-')
+loglog(narr,narr.^(-2),'o-')
+loglog(narr,narr.^(-3),'o-')
+loglog(narr,narr.^(-4),'o-')
+
 %%
 d=diff(uarr)
 d(1)/d(2)
 %%
-% x=-cos(linspace(0,pi,nx*20))'; y=x;
+x=-cos(linspace(0,pi,nx*5))'; y=x;
+% x=-cos(linspace(0,pi,nx))'; y=x;
 
 % uarr=[uarr;u((length(x)+1)/2)]
 uPhys=zeros(length(x)*length(y),1);
@@ -146,7 +185,7 @@ for ix=1:length(x)
         end
     end
 end
-%%
+%
 uPhys2=reshape(uPhys,[length(y),length(x)]);
 mesh(x,y,uPhys2);
 %%
--- cheb-diff/main_fd_2d.m
+++ cheb-diff/main_fd_2d.m
@@ -1,14 +1,17 @@
 clc; clear;
 uarr=[];
+%
+nx=11; 
+narr=[];
 %%
-nx=81; ny=nx;
-% x=linspace(-1,1,nx)'; y=x;
+ny=nx;
+x=linspace(-1,1,nx)'; y=x;
 % x=x.^3;
-x=-cos(linspace(0,pi,nx))';y=x;
+% x=-cos(linspace(0,pi,nx))';y=x;
 
 %d2udx2-1=0
 g=zeros(nx*ny,1);
-u=g;
+u=g-1;
 J=sparse(nx*ny,nx*ny,0);
 %
 % f=x*0-1*x.^2+1;
@@ -16,6 +19,11 @@ J=sparse(nx*ny,nx*ny,0);
 f=u*0-1;
 % f=cos(pi*x);
 %
+
+g=zeros(nx*ny,1);
+% u=g-1;
+J=sparse(nx*ny,nx*ny,0);
+
 for ix=1+1:length(x)-1
     for iy=1+1:length(y)-1
         dx1=x(ix)-x(ix-1); dx2=x(ix+1)-x(ix); dy1=y(iy)-y(iy-1); dy2=y(iy+1)-x(iy);
@@ -27,9 +35,12 @@ ipym=iy+(ix-1)*ny-1;
 
 g(ip)=g(ip)+-2/dx1/dx2*u(ip)+2/dx2/(dx1+dx2)*u(ipxp)+2/dx1/(dx1+dx2)*u(ipxm);
 g(ip)=g(ip)+-2/dy1/dy2*u(ip)+2/dy2/(dy1+dy2)*u(ipyp)+2/dy1/(dy1+dy2)*u(ipym);
-g(ip)=g(ip)+-f(ip);
+g(ip)=g(ip)+-f(ip)*cos(pi/2*x(ix))*cos(pi/2*y(iy));
+% g(ip)=g(ip)+-f(ip);
+% g(ip)=g(ip)+-u(ip)*pi*pi/4;
 
 J(ip,ip)=J(ip,ip)+-2/dx1/dx2-2/dy1/dy2;
+% J(ip,ip)=J(ip,ip)+-1*pi*pi/4;
 J(ip,ipxp)=J(ip,ipxp)+2/dx2/(dx1+dx2);
 J(ip,ipxm)=J(ip,ipxm)+2/dx1/(dx1+dx2);
 J(ip,ipyp)=J(ip,ipyp)+2/dy2/(dy1+dy2);
@@ -54,14 +65,24 @@ ix=nx; iy=ny; ip=iy+(ix-1)*ny; g(ip)=u(ip); J(ip,ip)=1;
     end
 
 %
+tic;
 u=u-J\g;
+toc;
+norm(g)
+%
 %
 uPhys=reshape(u,[ny,nx]);
 mesh(x,y,uPhys);
 %
 ix=(length(x)+1)/2; iy=(length(y)+1)/2; ip=iy+(ix-1)*ny; 
 uarr=[uarr;u(ip)]
+narr=[narr;nx];
+nx=(nx-1)*2+1;
 % anal=-1/pi/pi*2
 %%
 d=diff(uarr)
 d(1)/d(2)
+%%
+clf;
+loglog(narr,abs(uarr-2/pi/pi),'x-'); hold on;
+loglog(narr,narr.^(-2),'-')
